43a51398d86e2c0eaf0bedfd404f9bae61074d7d
Sanvi Aswal <aswal.sanvi8@gmail.com>
2025-04-21 19:28:05 +0530

    Add files via upload

---
+++ testwebdev/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <About />
+    </div>
+  );
+};
+
+export default App;
---
+++ testwebdev/src/components/About.jsx
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div className="about">
+      <img
+        src="C:\Users\ICAI\OneDrive\Desktop\react\portfolio\src\assets\portfolio.webp"
+        alt="Profile"
+        />
+      <h2 className="heading">Sanvi Aswal</h2>
+      <p>Hello! I'm Sanvi, a B.Tech ECE student at GTBIT. I enjoy working on AI, web development, and meaningful tech projects that solve real world problems.
+      </p>
+      <div>
+        <p>GitHub: <a href="https://github.com/SanviAswal23" >SanviAswal23</a></p>
+        <p>LinkedIn: <a href="https://linkedin.com/in/sanvi-aswal-a4a4ab313" >sanviaswal</a></p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
